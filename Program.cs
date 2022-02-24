c300b7066a02ad69c2766296ab1bbcce7a343144
Jørn Larsen <jqern.larsen@gmail.com>
2022-02-24 15:50:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+class Program
+{
+    /// <summary>
+    /// IDE for testmiljø
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main (string [] args)
+    {
+        RetailSystem system1 = new RetailSystem();
+
+        system1.MyShoppingChart =new List<RetailSystem.Item> ();
+
+        // PLU-koder for ItemList
+        var Item1 = new RetailSystem.Item();
+        Item1.Name = "Gummihansker";
+        Item1.Price = (decimal)59.90;
+        Item1.Count = 2;
+        Item1.IsPair = true;
+        Item1.KiloPrice = 0;
+        Item1.PLU = "A";
+
+        var Item2 = new RetailSystem.Item();
+        Item2.Name = "Stetoskop";
+        Item2.Price = 399;
+        Item2.Count = 1;
+        Item2.KiloPrice = 0;
+        Item2.PLU = "B";
+
+        var Item3 = new RetailSystem.Item();
+        Item3.Name = "Talkum";
+        Item3.Price = 0;
+        Item3.Count = 3000; // Pr. gram
+        Item3.KiloPrice = (decimal)19.54;
+        Item3.PLU = "C";        
+        
+        system1.MyShoppingChart.Add(Item1);
+        system1.MyShoppingChart.Add(Item2);
+        system1.MyShoppingChart.Add(Item3);
+
+        system1.ShopBasket(system1.MyShoppingChart);
+       
+    }
+
+
+
+
+}
+
+
---
+++ RetailSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class RetailSystem
+{
+
+    public List<Item> MyShoppingChart { get; set; }
+    public decimal Sum { get; set; }
+
+    public class Item
+    {
+
+        // navn på produkt
+        public string Name { get; set; }
+        // PLU for produkt
+        public string PLU { get; set; }
+        // Pris på produkt
+        public decimal Price { get; set; }
+        // Kilopris for produkt
+        public decimal KiloPrice { get; set; }
+        // Antall produkter i samling 
+        public int Count { get; set; }
+        // Er det par
+        public bool IsPair { get; set; }
+
+    }
+
+
+    public decimal ShopBasket(List<Item> inputItems) 
+    {
+        decimal NewSum = 0;
+
+
+        foreach (Item item in inputItems)
+        {
+            if (item.Price != 0 && item.KiloPrice == 0)
+            {
+                
+
+            }
+
+        }
+
+        return 0;
+    }
+}
+
+
